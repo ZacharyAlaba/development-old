168d3473994c6691ed5cc0bc7dcf126f47578ed2
ZacharyAlaba <zachary.alaba@urios.edu.ph>
2025-04-21 09:23:04 +0000

    New updated

---
+++ api/product_views.py
@@ -0,0 +1,16 @@
+
+from rest_framework import viewsets
+from .product_models import Product, Cart, Checkout
+from .serializers import ProductSerializer, CartSerializer, CheckoutSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CheckoutViewSet(viewsets.ModelViewSet):
+    queryset = Checkout.objects.all()
+    serializer_class = CheckoutSerializer
