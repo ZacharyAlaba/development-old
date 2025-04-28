0559b79af21f5472a48dcd175e0b165310e128de
ZacharyAlaba <zachary.alaba@urios.edu.ph>
2025-04-28 05:35:32 +0000

    ywaa

--- api/exam_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .exam_views import ChatView, ChatUpdateDetailView
-
-urlpatterns = [
-    path('chat/', ChatView.as_view(), name='chat_view'),
-    path('chats/<int:chat_id>/', ChatUpdateDetailView.as_view(), name='chat_update_detail'),
-]
